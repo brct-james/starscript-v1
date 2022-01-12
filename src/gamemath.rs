cad7a757a2418c1cb11c8aa1f07e3c4adc61c8c9
brct-james <james.clague@brct.io>
2022-01-12 22:25:46 +0000

    Split logic into modules

---
+++ src/gamemath.rs
@@ -0,0 +1,26 @@
+pub fn distance_from_coords(x1: &i32, y1: &i32, x2: &i32, y2: &i32) -> f64 {
+    f64::from((x2 - x1).pow(2) + (y2 - y1).pow(2)).sqrt()
+}
+
+pub fn calculate_flight_time(scale: &f64, speed: &f64, distance: &f64) -> f64 {
+    let docking_time = 30f64;
+    ((distance * scale) / speed) + docking_time
+}
+
+pub fn calculate_fuel_cost(distance: &f64, ship_type: &String, target_type: &String) -> i32 {
+    let mut fuel: i32;
+    if ship_type == "HM-MK-III" {
+        fuel = ((distance / 10f64).round() as i32) + 1;
+    } else {
+        fuel = ((distance / 7.5f64).round() as i32) + 1;
+    }
+    if target_type == "Planet" {
+        match ship_type.as_str() {
+            "MK-MK-III" => fuel += 1,
+            "GR-MK-II" => fuel += 3,
+            "GR-MK-III" => fuel += 4,
+            _ => fuel += 2,
+        }
+    }
+    return fuel;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
-use itertools::Itertools;
-use serde::{Deserialize, Serialize};
 use spacetraders::client::Client;
-use std::collections::{HashMap, HashSet};
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct MarketGoodSummary {
-    good_symbol: String,
-    location_symbol: String,
-    volume_per_unit: i32,
-    purchase_price_per_unit: i32,
-    sell_price_per_unit: i32,
-    quantity_available: i32,
-}
+use std::collections::HashMap;
+mod gamemath;
+mod traderoutes;
+use traderoutes::routes::{find_routes, MarketGoodSummary, Route};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -21,12 +12,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         "Greenitthe".to_string(),
         "4be25691-9594-4595-8344-ae3078b4b9fa".to_string(),
     );
-
-    // let info = client.get_my_info().await?;
-    // println!("Info: {:?}", info);
-
-    // let oe = client.get_location_info("OE-PM".to_string()).await?;
-    // println!("OE: {:?}", oe);
 
     let systems = client.get_systems_info().await?.systems;
     let oe_locs = &systems
@@ -51,7 +36,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             ),
         );
     }
-    // println!("{}", serde_json::to_string_pretty(&locs_info).unwrap());
 
     let mut mkt_symbols = HashMap::new();
     let mut mkts = HashMap::new();
@@ -83,14 +67,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     let ships_for_sale = client.get_ships_for_sale(&"OE".to_string()).await?.ships;
 
-    // println!("{}", serde_json::to_string_pretty(&mkts).unwrap());
-    // println!("{}", serde_json::to_string_pretty(&mkt_symbols).unwrap());
     // println!("{}", serde_json::to_string_pretty(&ships_for_sale).unwrap());
 
     // Find Potential Routes
-    find_routes(0i32, ships_for_sale, locs_info, mkt_symbols); //50i32
+    // HashMap is curated routes where ship_type is key
+    // Vec is top routes based on minimum_profit_per_time
+    let route: (HashMap<String, Route>, Vec<Route>) =
+        find_routes(50i32, ships_for_sale, locs_info, mkt_symbols);
+    println!("{}", serde_json::to_string_pretty(&route).unwrap());
 
-    // Calculate Fuel?
+    // Take top route, schedule ship to complete once
 
     // Score each based on $/time for each ship type (account for cargo size after fuel [and qty available], speed (flight time), price difference, and market share per run [dont want to be bringing 3000 to a station wanting 20 for ex])
 
@@ -98,249 +84,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
     // spacetraders::client::claim_username(client, "Greenitthe".to_string());
-}
-
-fn distance_from_coords(x1: &i32, y1: &i32, x2: &i32, y2: &i32) -> f64 {
-    f64::from((x2 - x1).pow(2) + (y2 - y1).pow(2)).sqrt()
-}
-
-fn calculate_flight_time(scale: &f64, speed: &f64, distance: &f64) -> f64 {
-    let docking_time = 30f64;
-    ((distance * scale) / speed) + docking_time
-}
-
-fn calculate_fuel_cost(distance: &f64, ship_type: &String, target_type: &String) -> i32 {
-    let mut fuel: i32;
-    if ship_type == "HM-MK-III" {
-        fuel = ((distance / 10f64).round() as i32) + 1;
-    } else {
-        fuel = ((distance / 7.5f64).round() as i32) + 1;
-    }
-    if target_type == "Planet" {
-        match ship_type.as_str() {
-            "MK-MK-III" => fuel += 1,
-            "GR-MK-II" => fuel += 3,
-            "GR-MK-III" => fuel += 4,
-            _ => fuel += 2,
-        }
-    }
-    return fuel;
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Eq, Ord, PartialEq, PartialOrd)]
-struct RouteFinancials {
-    purchase_price_at_start: i32,
-    sell_price_at_end: i32,
-    price_delta_per_unit: i32,
-    volume_per_unit: i32,
-    cargo_units_per_run: i32,
-    credits_per_run: i32,
-    credits_per_time: i32,
-    quantity_at_start: i32,
-    quantity_at_end: i32,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Eq, Ord, PartialEq, PartialOrd)]
-struct RouteWayfinding {
-    start_symbol: String,
-    start_type: String,
-    start_x: i32,
-    start_y: i32,
-    end_symbol: String,
-    end_type: String,
-    end_x: i32,
-    end_y: i32,
-    distance: i32,
-    flight_time: i32,
-    fuel_cost_to_end: i32,
-    fuel_cost_to_start: i32,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Eq, Ord, PartialEq, PartialOrd)]
-struct RouteShipInfo {
-    model: String,
-    speed: i32,
-    load_speed: i32,
-    cargo_size: i32,
-    cargo_restrictions: Vec<String>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Eq, Ord, PartialEq, PartialOrd)]
-struct Route {
-    name: String,
-    good: String,
-    financials: RouteFinancials,
-    wayfinding: RouteWayfinding,
-    ship_info: RouteShipInfo,
-}
-
-impl Route {
-    fn new(
-        good: &String,
-        ship: &spacetraders::shared::ShipForSale,
-        restricted_goods_string: Vec<String>,
-        start: &(String, String, i32, i32),
-        end: &(String, String, i32, i32),
-        goods_summary: &Vec<MarketGoodSummary>,
-    ) -> Route {
-        let ship_info = RouteShipInfo {
-            model: ship.ship_type.clone(),
-            speed: ship.speed.clone(),
-            load_speed: ship.loading_speed.clone(),
-            cargo_size: ship.max_cargo,
-            cargo_restrictions: restricted_goods_string,
-        };
-        let (start_symbol, start_type, start_x, start_y) = start;
-        let (end_symbol, end_type, end_x, end_y) = end;
-        let distance = distance_from_coords(start_x, start_y, end_x, end_y);
-        let flight_time = calculate_flight_time(&3f64, &(ship.speed as f64), &distance);
-        let fuel_cost_to_end = calculate_fuel_cost(&distance, &ship.ship_type, &end_type);
-        let fuel_cost_to_start = calculate_fuel_cost(&distance, &ship.ship_type, &start_type);
-        let wayfinding = RouteWayfinding {
-            start_symbol: start_symbol.to_string(),
-            start_type: start_type.to_string(),
-            start_x: *start_x,
-            start_y: *start_y,
-            end_symbol: end_symbol.to_string(),
-            end_type: end_type.to_string(),
-            end_x: *end_x,
-            end_y: *end_y,
-            distance: distance.round() as i32,
-            flight_time: flight_time.round() as i32,
-            fuel_cost_to_end: fuel_cost_to_end,
-            fuel_cost_to_start: fuel_cost_to_start,
-        };
-        let price_delta_per_unit =
-            goods_summary[1].sell_price_per_unit - goods_summary[0].purchase_price_per_unit;
-        let cargo_units_per_run = ship.max_cargo / goods_summary[0].volume_per_unit;
-        let credits_per_run = cargo_units_per_run * price_delta_per_unit;
-        let financials = RouteFinancials {
-            purchase_price_at_start: goods_summary[0].purchase_price_per_unit,
-            sell_price_at_end: goods_summary[1].sell_price_per_unit,
-            price_delta_per_unit: price_delta_per_unit,
-            volume_per_unit: goods_summary[0].volume_per_unit,
-            cargo_units_per_run: cargo_units_per_run,
-            credits_per_run: credits_per_run,
-            credits_per_time: (credits_per_run as f64 / flight_time).round() as i32,
-            quantity_at_start: goods_summary[0].quantity_available,
-            quantity_at_end: goods_summary[1].quantity_available,
-        };
-        let name = format!(
-            "{} | {} -> {} | {}",
-            good.to_string(),
-            start_symbol,
-            end_symbol,
-            ship.ship_type.to_string()
-        );
-        // Return
-        Route {
-            name: name,
-            good: good.to_string(),
-            ship_info: ship_info,
-            wayfinding: wayfinding,
-            financials: financials,
-        }
-    }
-}
-
-fn find_routes(
-    minimum_profit_per_time: i32,
-    ships_for_sale: Vec<spacetraders::shared::ShipForSale>,
-    locs_info: HashMap<String, (String, String, i32, i32)>,
-    mut goods: HashMap<String, Vec<MarketGoodSummary>>,
-) -> () {
-    let mut routes = Vec::<Route>::new();
-    // Filter tradable goods by requiring at least one in-system pair buying/selling each good
-    goods.retain(|_, v| (*v).len() >= 2);
-    let mut ship_models = HashSet::<String>::new();
-    // Collect all start/end permutations for remaining goods
-    for (goodname, good) in goods {
-        for endpoint_pair in good.into_iter().permutations(2) {
-            for ship in &ships_for_sale {
-                ship_models.insert(ship.ship_type.to_string());
-                let restricted = ship
-                    .restricted_goods
-                    .as_ref()
-                    .unwrap_or(&Vec::<::spacetraders::shared::Good>::new())
-                    .iter()
-                    .map(|&r| r.to_string())
-                    .collect::<Vec<String>>();
-                if restricted.len() == 0 || restricted.contains(&goodname) {
-                    let new_route = Route::new(
-                        &goodname,
-                        ship,
-                        restricted,
-                        &locs_info[&endpoint_pair[0].location_symbol],
-                        &locs_info[&endpoint_pair[1].location_symbol],
-                        &endpoint_pair,
-                    );
-                    if new_route.financials.credits_per_time > minimum_profit_per_time {
-                        routes.push(new_route);
-                    }
-                }
-            }
-        }
-    }
-
-    // Rank routes by best w.r.t. credits/time
-    let ranked_routes = rank_routes(routes);
-
-    // Curate routes by best w.r.t. ship type in credits/time
-    let routes_curated = curate_routes(ranked_routes.clone(), Vec::from_iter(ship_models));
-
-    // Get top routes regardless of ship type
-    let routes_top = filter_routes(ranked_routes.clone(), minimum_profit_per_time);
-
-    match save_routes("routes_curated.json".to_string(), routes_curated) {
-        Ok(_) => println!("Saved routes_curated"),
-        Err(why) => println!("An error occured while saving routes_curated: {}", why),
-    }
-    match save_routes("routes_top.json".to_string(), routes_top) {
-        Ok(_) => println!("Saved routes_top"),
-        Err(why) => println!("An error occured while saving routes_top: {}", why),
-    }
-}
-
-fn rank_routes(routes: Vec<Route>) -> Vec<Route> {
-    let mut ranked_routes = routes;
-    ranked_routes.sort_by(|a, b| {
-        b.financials
-            .credits_per_time
-            .cmp(&a.financials.credits_per_time)
-    });
-    return ranked_routes;
-}
-
-fn curate_routes(routes: Vec<Route>, ship_models: Vec<String>) -> Vec<Route> {
-    let mut routes_curated = Vec::<Route>::new();
-    for ship_model in ship_models {
-        let ship_routes = routes
-            .iter()
-            .filter(|route| route.ship_info.model == ship_model)
-            .cloned()
-            .collect::<Vec<Route>>();
-        routes_curated.push(ship_routes[0].clone());
-    }
-    return routes_curated;
-}
-
-fn filter_routes(routes: Vec<Route>, minimum_profit_per_time: i32) -> Vec<Route> {
-    let filtered_routes = routes
-        .iter()
-        .filter(|route| route.financials.credits_per_time > minimum_profit_per_time)
-        .cloned()
-        .collect::<Vec<Route>>();
-    return filtered_routes;
-}
-
-fn save_routes(filename: String, routes: Vec<Route>) -> Result<(), Box<dyn std::error::Error>> {
-    let f = std::fs::OpenOptions::new()
-        .truncate(true)
-        .write(true)
-        .create(true)
-        .open(filename)?;
-    // write to file with serde
-    serde_json::to_writer_pretty(f, &routes)?;
-
-    Ok(())
 }
